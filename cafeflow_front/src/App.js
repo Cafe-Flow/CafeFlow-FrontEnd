f411182a72f9ff0988e197177d502dbd585790e0
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-03-30 22:04:01 +0900

    [ADD] :white_check_mark: 메인 화면 슬라이드 / 화면 미디어 쿼리 설정

--- cafeflow_front/src/App.js
+++ cafeflow_front/src/App.js
@@ -1,12 +1,23 @@
-
 import './App.css';
-
+import Header from './Header.js';
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './MainPage.js';
 function App() {
   return (
     <div className="App">
+            <Header/>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/detail' element={<div>detail</div>}/>
+        <Route path='/mypage' element={<div>mypage</div>}/>
+      </Routes>
 
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ cafeflow_front/src/MainPage.js
@@ -0,0 +1,59 @@
+import './App.css';
+import { Card, CardBody, Carousel, Button } from 'react-bootstrap';
+
+function MainPage() {
+  return (
+    <div className="App">
+      <Carousel>
+        <Carousel.Item>
+        <Card className='custom-card' style={{ backgroundColor: '#43f770' }}>
+        <CardBody className="custom-card-body">
+        <div className="text-container">
+              <p className="card-title-custom">실시간 카페<br/>좌석을 확인!</p>
+              <p className="card-text-custom">
+              가고싶은 카페의 실시간 남은 자리를 확인해봐요
+              </p>
+              <Button className="card-button" style={{ fontFamily: "'Roboto', sans-serif", fontSize: '20px',
+              fontWeight: 400, backgroundColor: 'white', color: 'black', borderColor: 'white' }}>지금 보러가기 👉</Button>
+            </div>
+            <img src="/슬라이드2.png" className="custom-image" alt="first pic" />
+            </CardBody>
+          </Card>
+        </Carousel.Item>
+        <Carousel.Item>
+        <Card className='custom-card' style={{ backgroundColor: '#43dcf7' }}>
+        <CardBody className="custom-card-body">
+        <div className="text-container">
+              <p className="card-title-custom">언제 어디서든 <br/> 편하게 주문!</p>
+              <p className="card-text-custom">
+              어디서나 편하게 비대면으로 주문해봐요
+              </p>
+              <Button className="card-button" style={{ fontFamily: "'Roboto', sans-serif", fontSize: '20px',
+              fontWeight: 400, backgroundColor: 'white', color: 'black', borderColor: 'white' }}>지금 결제하기 👉</Button>
+            </div>
+            <img src="/슬라이드1.png" className="custom-image" alt="second pic" />
+            </CardBody>
+          </Card>
+        </Carousel.Item>
+        <Carousel.Item>
+        <Card className='custom-card' style={{ backgroundColor: '#4D5152' }}>
+        <CardBody className="custom-card-body">
+        <div className="text-container">
+              <p className="card-title-custom" style={{color: 'white'}}>최애 카페를<br/> 함께 공유!</p>
+              <p className="card-text-custom"style={{color: 'white'}} >
+                사람들에게 알리고 싶은 나만의 최애 카페를 공유 해봐요
+              </p>
+              <Button className="card-button" style={{ fontFamily: "'Roboto', sans-serif", fontSize: '20px',
+              fontWeight: 400, backgroundColor: 'white', color: 'black', borderColor: 'white' }}>지금 하러가기 👉</Button>
+            </div>
+            <img src="/슬라이드3.png" className="custom-image" alt="설명" />
+            </CardBody>
+          </Card>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+
+export default MainPage;
