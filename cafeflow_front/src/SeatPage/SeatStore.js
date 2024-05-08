29993066c9af82cca1c084bf2575710d13e287e6
lunghyun <103187357+lunghyun@users.noreply.github.com>
2024-05-08 15:20:35 +0900

    [ADD] 좌석 기능 수정

---
+++ cafeflow_front/src/SeatPage/SeatStore.js
@@ -0,0 +1,8 @@
+import create from 'zustand';
+
+const useSeatStore = create((set) => ({
+  seats: [],
+  setSeats: (newSeats) => set({ seats: newSeats }),
+}));
+
+export default useSeatStore;
--- cafeflow_front/src/SeatPage/SeatView.js
+++ cafeflow_front/src/SeatPage/SeatView.js
@@ -1,55 +1,40 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import "./SeatRegister.css";
+// SeatView.js
 
-function SeatView({ idx }) { // idx prop을 받아옴
-  const [seats, setSeats] = useState([]);
-  
-  useEffect(() => {
-    const fetchSeats = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8080/api/cafe/${idx}/seat-register`
-        );
-        setSeats(response.data);
-      } catch (error) {
-        console.error("Error fetching seats:", error); 
-      }
-    };
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './SeatView.css'; // You can define your own CSS for SeatView
 
-    fetchSeats();
-  }, [idx]); // idx가 변경될 때마다 호출
+function SeatView({ seats }) { // Accept idx as a prop
 
-  return (
-    <div id="background">
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div id="outerContainer">
-          <div id="container">
-            {seats.map(seat => (
-              <div
-                key={seat.id}
-                className="draggable"
-                style={{
-                  width: seat.seatAngle === "HORIZONTAL" ? 100 : seat.seatSize * 50,
-                  height: seat.seatAngle === "HORIZONTAL" ? seat.seatSize * 50 : 100,
-                  left: seat.seatCoordinates.x,
-                  top: seat.seatCoordinates.y
-                }}
-              >
-                {seat.seatNumber}
-                <button className="rotate-btn" onClick={() => console.log('Rotate seat:', seat.id)}>↻</button>
-                <button className="delete-btn" onClick={() => console.log('Delete seat:', seat.id)}>X</button>
-              </div>
-            ))}
-          </div>
+    return (
+        <div id="background">
+            <div style={{ display: "flex", flexDirection: "row", transform: "scale(0.8)" }}>
+                <div id="outerContainer">
+                    <div id="container">
+                      <div class="seatlog"></div>
+                        {seats.map(seat => (
+                            <div
+                                key={seat.id}
+                                className="draggable"
+                                style={{
+                                    width: seat.seatAngle === "HORIZONTAL" ? 100 : seat.seatSize * 50,
+                                    height: seat.seatAngle === "HORIZONTAL" ? seat.seatSize * 50 : 100,
+                                    left: seat.seatCoordinates.x,
+                                    top: seat.seatCoordinates.y,
+                                    backgroundColor: seat.seatHasPlug ? "#1abc9c" : "#d3c2a0"
+                                }}
+                            >
+                                {seat.seatNumber}
+                                <div>
+                                    Position: {seat.seatCoordinates.x}, {seat.seatCoordinates.y}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SeatView;
