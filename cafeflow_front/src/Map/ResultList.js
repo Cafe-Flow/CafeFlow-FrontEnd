54301b82801c14c1eed58322bbefd17fdd71ef96
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-06-11 05:07:45 +0900

    [ADD] 네이버 MAP 실시간 혼잡도 조회

--- cafeflow_front/src/Map/ResultList.js
+++ cafeflow_front/src/Map/ResultList.js
@@ -4,14 +4,17 @@ import { Link } from "react-router-dom";
 import MainChat from "../Chat/MainChat";
 import ChatPopup from "../Chat/ChatPopUp";
 
-function getCongestionLevel(congestion) {
-  return congestion > 80 ? "혼잡" : congestion > 50 ? "적정" : "원활";
+function getCongestionLevel(traffic) {
+  if (traffic === "RED") {
+    return "혼잡";
+  } else if (traffic === "BLUE") {
+    return "적정";
+  } else if (traffic === "GREEN") {
+    return "원활";
+  } else {
+    return "적정";
+  }
 }
-
-function getCongestionColor(congestion) {
-  return congestion > 80 ? "red" : congestion > 50 ? "blue" : "green";
-}
-
 function ResultList({ markersData, onMarkerClick }) {
   const allResults = markersData;
   const [hoveredItem, setHoveredItem] = useState(null);
@@ -54,11 +57,11 @@ function ResultList({ markersData, onMarkerClick }) {
             <li key={item.id} onClick={() => onMarkerClick(item)}>
               <span className="si-goo">{item.address}</span>
               <span className="maejang-name">
-                {item.name} - {getCongestionLevel(item.congestion)}{" "}
+                {item.name} - {getCongestionLevel(item.traffic)}{" "}
                 <span
                   className="congestion-indicator"
                   style={{
-                    backgroundColor: getCongestionColor(item.congestion),
+                    backgroundColor: item.traffic,
                     marginLeft: "10px",
                   }}
                 ></span>
@@ -67,7 +70,7 @@ function ResultList({ markersData, onMarkerClick }) {
                     hoveredItem === item.id ? "visible" : ""
                   }`}
                 >
-                  {item.description}
+                  {item.detailAddress}
                 </div>
               </span>
               <Link to={`/shop/${item.id}`}>
